05cf0d95c48b3b80d4576cc8b5258e526df583b0
LouisNgatale <louisngatale@gmail.com>
2021-03-27 13:54:43 +0300

    first commit

---
+++ src/main/java/io/ngatale/employee_authentication/EmployeeAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package io.ngatale.employee_authentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeAuthenticationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeAuthenticationApplication.class, args);
+    }
+
+}
